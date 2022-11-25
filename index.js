77480075d6dd0ce1c3567c03981d94f43107e907
Glen <glen.kimani@student.moringaschool.com>
2022-11-26 00:32:32 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+var customerName = ("bob");
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob' 
+}
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'we are tired';
+    leastFavoriteCustomer = 'we are sleeping'
+}
 // Write your solution in this file!
